edefc0b66de9da40d8f5799b99f8255f51b9db77
sebastienlanglois <sebastien.langlois@polymtl.ca>
2023-05-08 11:56:13 -0400

    first commit

--- pipeline.py
+++ pipeline.py
@@ -1,158 +1,175 @@
-from prefect import task, Flow, case, agent
-import prefect
-from prefect.schedules import IntervalSchedule
+from prefect import task, Flow
 import pandas as pd
-from datetime import datetime, timedelta
-import urllib.request
-import s3fs
 import os
-import subprocess
-import s3fs
-import zarr
+import xarray as xr
+from prefect.executors import DaskExecutor
+from pangeo_forge_recipes.storage import FSSpecTarget
+from fsspec.implementations.local import LocalFileSystem
+import shutil
+import fsspec
 
-from pipeline.models.hydat import get_available_stations_from_hydat, import_hydat_to_parquet, verify_data_type_exists
-from prefect.utilities.configuration import set_temporary_config
-from pipeline.utils import get_url_paths
+from config import Config
+from models import get_available_stations_from_cehq, StationParserCEHQ
 
+
+def prepare_id_metadata(station):
+    """
+    Prepare id metadata
+
+    """
+    metadata_names = ["id", "name", "province", "longitude", "latitude",
+                      "drainage_area", "regulated"]
+    
+    ds = pd.DataFrame(index=metadata_names,
+                        data=[getattr(station, attr)
+                              for attr in metadata_names]).T.set_index('id').to_xarray()
+    
+    ds['source'] = ("source", [station.source])
+    ds['timestep'] = ("timestep", [station.timestep])
+    ds['time_agg'] = ("time_agg", [station.time_agg])
+    ds['spatial_agg'] = ("spatial_agg", [station.spatial_agg])
+    
+    ds['drainage_area'].attrs = {
+        "long_name": "drainage_area",
+        "units": "km2",
+        }
+
+    ds['latitude'].attrs = {
+        "standard_name": "latitude",
+        "long_name": "latitude",
+        "units": "decimal_degrees",
+            }
+    ds['longitude'].attrs = {
+        "standard_name": "longitude",
+        "long_name": "longitude",
+        "units": "decimal_degrees",
+        }
+    
+    metadata_names.append('source')
+    
+    ds = ds.set_coords(metadata_names)
+
+    return ds 
+
+
+def prepare_values(station):
+        
+    ds_time = pd.DataFrame(columns=['time'],
+             data=pd.date_range('1860-01-01', pd.Timestamp.now())).set_index('time').to_xarray()
+
+    ds_values = station.values.to_xarray().rename({'value': station.data_type})
+    ds_values[station.data_type] = ds_values[station.data_type].expand_dims(['id', 'spatial_agg', 'timestep', 'time_agg', 'source'])
+    ds_values = ds_values.transpose('time','id', 'spatial_agg', 'timestep', 'time_agg', 'source')
+    return xr.merge([ds_values, ds_time])
+
+
+def postprocess(ds, data_type):
+    ds = ds.chunk({'id': 1, 'time_agg': 1, 'timestep': 1, 'time': -1, 'spatial_agg': 1})
+
+    for var in ds.variables:
+        ds[var].encoding.clear()
+
+    ds['start_date'] = ds[data_type].dropna('time').isel(time=[0]).idxmax('time').expand_dims({'variable': [data_type]})
+    ds['end_date'] = ds[data_type].dropna('time').isel(time=[-1]).idxmax('time').expand_dims({'variable': [data_type]})
+    ds = ds.set_coords(['start_date','end_date'])
+
+    ds['name'] = ds.name.astype(object)
+    ds['id'] = ds.id.astype(object)
+    ds['latitude'] = ds.latitude.astype('float32')
+    ds['longitude'] = ds.longitude.astype('float32')
+    ds['province'] = ds.province.astype(object)
+    ds['drainage_area'] = ds.drainage_area.astype('float32')
+    ds['regulated'] = ds.regulated.astype(object)
+    ds['source'] = ds.source.astype(object)
+    ds['timestep'] = ds.timestep.astype(object)
+    ds['spatial_agg'] = ds.spatial_agg.astype(object)
+    ds['time_agg'] = ds.time_agg.astype(object)
+    ds['variable'] = ds.variable.astype(object)
+    ds[data_type] = ds[data_type].astype('float32')
+
+    ds[data_type] = ds[data_type].transpose('id', 'time', 'spatial_agg', 'timestep', 'time_agg', 'source')
+    
+    return ds
 
 @task
-def extract_hydat_path(url, ext):
+def get_available_stations():
     # fetch reference data
-    path = get_url_paths(url=url, ext=ext)
-    return path
+    stations_list = get_available_stations_from_cehq()
+    return stations_list[0:2]
+
+
 
 @task
-def verify_if_to_date(path):
-    # fetch reference data
-    # Wasabi cloud storage configurations
-    basename = os.path.basename(path)
-    client_kwargs = {'endpoint_url': 'https://s3.us-east-2.wasabisys.com',
-                     'region_name': 'us-east-2'}
-    config_kwargs = {'max_pool_connections': 30}
+def store_station(input_path):
+    """
 
-    s3 = s3fs.S3FileSystem(profile='default',
-                           client_kwargs=client_kwargs,
-                           config_kwargs=config_kwargs)  # public read
+    :param station:
+    :return:
+    """
+    CEHQ_URL = "https://www.cehq.gouv.qc.ca/depot/historique_donnees/fichier/"
+    output_path = '/tmp/deh/raw'
 
-    return s3.exists(os.path.join('s3://hydat-sqlite',
-                                  basename))
-
-@task
-def download_hydat_file(path):
-    # fetch reference data
-    # Wasabi cloud storage configurations
-    basename = os.path.basename(path)
-
-    urllib.request.urlretrieve(os.path.join("https://collaboration.cmc.ec.gc.ca/cmc/hydrometrics/www",
-                                            basename),
-                               os.path.join('/tmp',
-                                            basename))
-
-    client_kwargs = {'endpoint_url': 'https://s3.us-east-2.wasabisys.com',
-                     'region_name': 'us-east-2'}
-    config_kwargs = {'max_pool_connections': 30}
-
-    s3 = s3fs.S3FileSystem(profile='default',
-                           client_kwargs=client_kwargs,
-                           config_kwargs=config_kwargs)  # public read
-
-    s3.put(os.path.join('/tmp',
-                        basename),
-           os.path.join('s3://hydat-sqlite',
-                        basename))
-    return path
+    try:
+        station_id = input_path.strip()
+        station = StationParserCEHQ(url=CEHQ_URL + os.path.basename(station_id) + '_Q.txt')
+        ds_meta = prepare_id_metadata(station)
+        ds_values = prepare_values(station)
+        ds = xr.merge([ds_values, ds_meta])
+        ds = postprocess(ds, station.data_type)
+        ds.to_zarr(os.path.join(output_path, station_id + "_Q"), consolidated=True)
+    except Exception:
+        pass
+    try:
+        station_id = input_path.strip()
+        station = StationParserCEHQ(CEHQ_URL + os.path.basename(station_id) + '_N.txt')
+        ds_meta = prepare_id_metadata(station)
+        ds_values = prepare_values(station)
+        ds = xr.merge([ds_values, ds_meta])
+        ds = postprocess(ds, station.data_type)
+        ds.to_zarr(os.path.join(output_path, station_id +  "_N"), consolidated=True)
+    except Exception:
+        pass
 
 @task
-def update_hydat_database(path):
-    project_root = '/tmp'
-    data_dir = os.path.join(project_root, 'data')
-    stations_list = get_available_stations_from_hydat()
-    #
-    # results = []
-    for station_number in stations_list:
-        if verify_data_type_exists(station_number, 'Flow'):
-            import_hydat_to_parquet(station_number)
+def merge_stations():
+    ds = xr.open_mfdataset('/tmp/deh/raw/*', 
+                    engine='zarr',
+                    consolidated=True, 
+                    parallel=True)
+    ds = ds.chunk({'id': 1, 'time_agg': 1, 'timestep': 1, 'time': -1, 'spatial_agg': 1})
+    for var in ds.variables:
+        ds[var].encoding.clear()
 
-    storage_options = {"client_kwargs": {'endpoint_url': 'https://s3.us-east-2.wasabisys.com',
-                                         'region_name': 'us-east-2'}}
+    ds['name'] = ds.name.astype(object)
+    ds['id'] = ds.id.astype(object)
+    ds['latitude'] = ds.latitude.astype('float32')
+    ds['longitude'] = ds.longitude.astype('float32')
+    ds['province'] = ds.province.astype(object)
+    ds['drainage_area'] = ds.drainage_area.astype('float32')
+    ds['regulated'] = ds.regulated.astype(object)
+    ds['source'] = ds.source.astype(object)
+    ds['timestep'] = ds.timestep.astype(object)
+    ds['spatial_agg'] = ds.spatial_agg.astype(object)
+    ds['time_agg'] = ds.time_agg.astype(object)
+    ds['variable'] = ds.time_agg.astype(object)
+    
+        
+    ds.to_zarr('/tmp/deh/timeseries', consolidated=True)
 
-    df = pd.read_parquet(os.path.join(data_dir, 'basin.parquet'), engine='pyarrow')
-    df.to_parquet('s3://hydrology/timeseries/sources/hydat/basin.parquet',
-                  engine='fastparquet',
-                  compression='gzip',
-                  storage_options=storage_options)
-    df = pd.read_parquet(os.path.join(data_dir, 'context.parquet'), engine='pyarrow')
-    df.to_parquet('s3://hydrology/timeseries/sources/hydat/context.parquet',
-                  engine='fastparquet',
-                  compression='gzip',
-                  storage_options=storage_options)
+@ task()
+def push_data_to_bucket():
+    lfs = LocalFileSystem()
+    target = FSSpecTarget(fs=lfs, root_path='/tmp/deh/timeseries')
+    fs = fsspec.filesystem('s3', **Config.STORAGE_OPTIONS)
+    fs.put(target.root_path, os.path.dirname(Config.DEH_ZARR_BUCKET), recursive=True)
+    shutil.rmtree(target.root_path)
 
-    client_kwargs = {'endpoint_url': 'https://s3.us-east-2.wasabisys.com',
-                     'region_name': 'us-east-2'}
-    config_kwargs = {'max_pool_connections': 30}
+if __name__ == '__main__':
+    with Flow("DEH-ETL", executor=DaskExecutor()) as flow:
 
-    bucket_source = os.path.join(data_dir, 'zarr')
-    bucket_sink = "s3://hydrology/timeseries/sources/hydat/values.zarr "
-    endpoint_url = 'https://s3.us-east-2.wasabisys.com'
-    region='us-east-2'
+        stations_list = get_available_stations()
+        store = store_station.map(stations_list)
+        merged = merge_stations(upstream_tasks=[store])
+        push_data_to_bucket(upstream_tasks=[merged])
 
-    s3 = s3fs.S3FileSystem(client_kwargs=client_kwargs,
-                           config_kwargs=config_kwargs)
-    store = s3fs.S3Map(root=bucket_sink,
-                       s3=s3)
-
-
-    aws_command = "aws s3 sync {} {} --endpoint-url={} --region={}".format(bucket_source,
-                                                                           bucket_sink,
-                                                                           endpoint_url,
-                                                                           region)
-    print(aws_command)
-    subprocess.call(aws_command, shell=True)
-
-    zarr.consolidate_metadata(store)
-
-
-# schedule to run every 12 hours
-schedule = IntervalSchedule(
-    start_date=datetime.utcnow() + timedelta(seconds=1),
-    interval=timedelta(hours=12),
-    end_date=datetime.utcnow() + timedelta(seconds=20))
-
-# import pendulum
-#
-# from prefect.schedules import Schedule
-# from prefect.schedules.clocks import DatesClock
-#
-# schedule = Schedule(
-#     clocks=[DatesClock([pendulum.now().add(seconds=1)])])
-
-temp_config = {
-    "cloud.agent.auth_token": prefect.config.cloud.agent.auth_token,
-}
-
-
-with Flow("Hydat-ETL") as flow:
-
-    with set_temporary_config(temp_config):
-        if flow.run_config is not None:
-            labels = list(flow.run_config.labels or ())
-        elif flow.environment is not None:
-            labels = list(flow.environment.labels or ())
-        else:
-            labels = []
-        agent = agent.local.LocalAgent(
-            labels=labels, max_polls=50
-        )
-
-    url = 'https://collaboration.cmc.ec.gc.ca/cmc/hydrometrics/www/'
-    ext = 'zip'
-
-    path = extract_hydat_path(url, ext)
-    cond = verify_if_to_date(path)
-    with case(cond, False):
-        path = download_hydat_file(path)
-        update_hydat_database(path)
-
-flow.register(project_name="hydat-file-upload")
-# agent.start()
-flow.run()
+    flow.run()
